5733e439a052b500862434b06a38190a574913eb
LaurentDardenne <LaDardenne@Orange.fr>
2017-04-22 20:04:04 +0200

    First of all, update the PowershellGet

---
+++ src/UpdatePowershellGet.ps1
@@ -0,0 +1,12 @@
+﻿ #nouvelle session pour installer PowershellGet
+PowerShell.exe -Command {
+  Install-PackageProvider Nuget -ForceBootstrap -Force > $null
+  Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+  Install-module PowershellGet -MinimumVersion 1.1.0.0 -Force -Scope AllUsers 
+} 
+
+Install-Module Psake -force
+Import-Module Psake
+
+ #Install/Update required modules
+. "$PSScriptAnalyzerRulesVcs\Tools\Install.ps1"
