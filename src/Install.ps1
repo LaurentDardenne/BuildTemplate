fc8c3fd559ab62d399b24a9a18cb593abff7bd77
LaurentDardenne <LaDardenne@Orange.fr>
2017-04-22 19:53:30 +0200

    Task to install or update the dependencies of the build

---
+++ src/Install.ps1
@@ -0,0 +1,14 @@
+﻿#Install.ps1
+#Install or update the dependencies (Modules, scripts, binaries)
+ [CmdletBinding(DefaultParameterSetName = "Install")]
+ Param(
+     [Parameter(ParameterSetName="Update")]
+   [switch] $Update
+ )
+#By default we install, otherwise we update
+#And we force the install for the CI
+if (Test-Path env:APPVEYOR)
+{ Invoke-Psake "$PSScriptAnalyzerRulesVcs\Tools\Dependencies.ps1" -parameters @{"Mode"="Install"} -nologo }
+else
+{ Invoke-Psake "$PSScriptAnalyzerRulesVcs\Tools\Dependencies.ps1" -parameters @{"Mode"="$($PsCmdlet.ParameterSetName)"} -nologo }
+
